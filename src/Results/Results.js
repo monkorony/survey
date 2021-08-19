8773f0f984f32bd48ba9ebcbda44c8968bf3ce36
Sirin Srikier <sirinsrikier@Sirins-MacBook-Pro.local>
2021-08-18 19:45:12 -0700

    update results

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -8,20 +8,12 @@ const Results = ({
   surveyData
 }) => {
 
-  let fakeResult = [
-    { 0: ["your_fitness_goal_relieve_stress", "your_fitness_goal_prep_for_an_event"] },
-    { 1: ["every_day", "5-6x_per_week"] },
-  ]
-  const keys = Object.keys(fakeResult);
-  let surveyAnswersArr = JSON.parse(localStorage.getItem("userInput"));
-  console.log(keys, 'keys');
-  console.log(fakeResult)
   return (
     <div className="results">
-      {keys.map(key => (
-        <div key={key}>{surveyData[key].question}</div>
-      ))}
-      
+      <div>Based on your responses, we recommend the these programs:</div>
+      <ul>
+        <li>Strength Training</li>
+      </ul>
     </div>
   )
 }
